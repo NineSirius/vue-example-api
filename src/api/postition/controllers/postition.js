4ed92d9c939af7cc3d158c112a550bfbb29deac4
Ruslan Bulah <ruslanbulah3@gmail.com>
2023-04-14 00:25:48 +0600

    api for vue-axample

---
+++ src/api/postition/controllers/postition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * postition controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::postition.postition');
---
+++ src/api/postition/routes/postition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * postition router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::postition.postition');
